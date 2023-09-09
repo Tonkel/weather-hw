68cef07c0fe7a0e2d6cf4a121ba868f44f9398a1
Tonkel <w.tucker.cac@gmail.com>
2023-09-09 14:49:39 -0400

    adds functionality that allows user to see current weather metrics displayed when they search a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,55 @@
 var searchInputEl = document.getElementById("search-input");
 var searchButtonEl = document.getElementById("search-button");
 var recentSearchesEl = document.getElementById("recent-searches");
+var currentDateEl = document.getElementById("current-date");
+var currentTempEl = document.getElementById("current-temp");
+var currentWindEl = document.getElementById("current-wind");
+var currentHumidEl = document.getElementById("current-humid");
+//data
+var date = dayjs().format(`M/D/YYYY`);
+
 //functions
 function appendToList(cityName) {
   var newListItem = recentSearchesEl.appendChild(document.createElement("li"));
   var listButton = newListItem.appendChild(document.createElement("button"));
   listButton.textContent = cityName;
+  listButton.setAttribute("id", "list-button");
+}
+
+//fetch API
+function fetchAPI(API) {
+  return fetch(API)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data;
+    });
 }
 
 //event listener
 searchButtonEl.addEventListener("click", function () {
   var searchinput = searchInputEl.value;
-  console.log(searchinput);
+
+  //appends search input to list
   appendToList(searchinput);
+
+  var currentWeatherAPIKey = `https://api.openweathermap.org/data/2.5/weather?q=${searchinput}&appid=e9fd4d66744477a7c633b9e44349f6b3&units=imperial`;
+
+  //fetch and manipulate current weather data
+  fetchAPI(currentWeatherAPIKey).then(function (data) {
+    console.log(data);
+    //data
+    var wind = data.wind.speed;
+    var temp = data.main.temp;
+    var humidity = data.main.humidity;
+
+    //manipulate elements
+    currentDateEl.textContent = `${searchinput} (${date})`;
+    currentWindEl.textContent = `Wind: ${wind}mph`;
+    currentHumidEl.textContent = `Humidity: ${humidity}`;
+    currentTempEl.textContent = `Temp: ${temp} degrees`;
+  });
 });
 //create event listener for search button to take text input
 //create function that takes the weather from that city and displays it in the divs created
