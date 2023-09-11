4809ebbd4dd8773dd18ecb977b135f52ba91b32a
Tonkel <w.tucker.cac@gmail.com>
2023-09-11 15:13:03 -0400

    adds function that creates elements to display weather forecast for a few days

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ var currentDateEl = document.getElementById("current-date");
 var currentTempEl = document.getElementById("current-temp");
 var currentWindEl = document.getElementById("current-wind");
 var currentHumidEl = document.getElementById("current-humid");
+var forecastContainer = document.getElementsByClassName("container-4");
 //data
 var date = dayjs().format(`M/D/YYYY`);
 
@@ -41,7 +42,6 @@ searchButtonEl.addEventListener("click", function () {
 
   //fetch and manipulate current weather data
   fetchAPI(currentWeatherAPIKey).then(function (data) {
-    console.log(data);
     //data
     var wind = data.wind.speed;
     var temp = data.main.temp;
@@ -53,7 +53,54 @@ searchButtonEl.addEventListener("click", function () {
     currentHumidEl.textContent = `Humidity: ${humidity}`;
     currentTempEl.textContent = `Temp: ${temp} degrees`;
   });
+
+  //now get data for 5 day forecast
+  var weatherForecastKey = `https://api.openweathermap.org/data/2.5/forecast?q=${searchinput}&appid=e9fd4d66744477a7c633b9e44349f6b3&units=imperial`;
+
+  fetchAPI(weatherForecastKey).then(function (data) {
+    console.log(data);
+    dataList = data.list;
+    //for statement to isolate only the 12pm timestamps
+    for (obj in dataList) {
+      if (dataList[obj].dt_txt.includes("12:00:00")) {
+        console.log(dataList[obj]);
+        //data
+        var date2 = dayjs(dataList[obj].dt_txt).format("M/D/YYYY");
+        var icon = dataList[obj].weather[0].icon;
+        var temp = dataList[obj].main.temp;
+        var wind = dataList[obj].wind.speed;
+        var humidity = dataList[obj].main.humidity;
+        //create div to put in flex container
+        var divEl = document.createElement("div");
+        //build
+        divEl.setAttribute("class", ".card");
+        //place
+        forecastContainer[0].appendChild(divEl);
+
+        var dateEl = document.createElement("h3");
+        dateEl.textContent = date2;
+        divEl.appendChild(dateEl);
+
+        var iconEl = document.createElement("i");
+        iconEl.innerHTML = icon;
+        divEl.appendChild(iconEl);
+
+        var tempEL = document.createElement("p");
+        tempEL.textContent = temp;
+        divEl.appendChild(tempEL);
+
+        var windEl = document.createElement("p");
+        windEl.textContent = wind;
+        divEl.appendChild(windEl);
+
+        var humidEl = document.createElement("p");
+        humidEl.textContent = humidity;
+        divEl.appendChild(humidEl);
+      }
+    }
+  });
 });
+
 //create event listener for search button to take text input
 //create function that takes the weather from that city and displays it in the divs created
 // create function that takes the 5day forecast and displays it
